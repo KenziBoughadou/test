0a1af92e27970c2f492dd676b48bf43080a8de4a
Boughadou Kenzi <74599039+KenziBoughadou@users.noreply.github.com>
2025-06-30 19:32:44 +0200

    Delete api directory

--- api/curl.sh
+++
@@ -1,2 +0,0 @@
-curl -H "Authorisation:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFudG9pbmVAZ21haWwuY29tIiwiaWF0IjoxNzUxMjgwNjk5LCJleHAiOjE3NTEyODQyOTl9.xUX3fmzE54AedWWbcgL9eIuJXWjYYw9qdK9yVYjbLbc" \
-    -X DELETE "http://localhost:3000/api/v1/users/delete"
